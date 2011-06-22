1c5608f7d78c42fca282ef6871120294d906eee3
cincinnatusc123 <cincinnatusc123@gmail.com>
2011-06-21 22:05:19 -0700

    stupid typo in GlobalConfig.pm that didn't cause an error until I tried on bix?

--- lib/GlobalConfig.pm
+++ lib/GlobalConfig.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Exporter 'import';
 
-our @EXPORT = qw($root_dir $data_dir $upload_dir $DEBUG $INTERESTING_AREAS_GAP_THRESH );
+our @EXPORT = qw($root_dir $data_dir $upload_dir $DEBUG $INTERESTING_AREAS_GAP_THRESH);
 use JSON 2;
 
 use Cwd;
@@ -22,8 +22,9 @@ $json_text =~ s/^.+= //;
     
 close GC;
 
-open(DEBUG,">","debug_globals.txt");
-print DEBUG "asdfsdfasdf\n";
+#open(DEBUGFILE,">","debug_globals.txt");
+#print DEBUGFILE "asdfsdfasdf\n";
+
 my $globals = JSON::decode_json($json_text);
 
 
@@ -31,17 +32,12 @@ our $root_dir = $globals->{root_dir};
 our $data_dir = $root_dir . $globals->{data_dir};
 our $upload_dir = $root_dir . $globals->{upload_dir};
 our $DEBUG = $globals->{DEBUG};
-our $INTERESTING_AREAS_GAP_THRESH = $globals->{INTERESTING_AREAS_GAP_THESH};
+our $INTERESTING_AREAS_GAP_THRESH = $globals->{INTERESTING_AREAS_GAP_THRESH};
+#our $FAKING_REFSEQ = $globals->{FAKING_REFSEQ};
 
-print DEBUG "$globals\n";
-print DEBUG "rootdir: $root_dir\n";
-print DEBUG "uploaddir: $upload_dir\n";
-close(DEBUG);
-
-#our $root_dir = "/home/andrew/school/dnavis/jbrowse";
-#our $data_dir = $root_dir . "/data";
-#our $upload_dir = $root_dir . "/uploads";
-#our $DEBUG = 1;
-#our $BOOKMARK_THRESH = 5000;
+#print DEBUGFILE "$globals\n";
+#print DEBUGFILE "rootdir: $root_dir\n";
+#print DEBUGFILE "uploaddir: $upload_dir\n";
+#close(DEBUGFILE);
 
 1;
