b17fa55f35048a239ff1a6814b3ad04c2b3d1170
andrew <andrew@rodion.(none)>
2011-08-28 09:46:28 -0700

    stripped down

--- genomequery/biosql_compiler/biosql/run_biosql.sh
+++ genomequery/biosql_compiler/biosql/run_biosql.sh
@@ -11,8 +11,8 @@ back_end_dir="back_end"
 src_table_dir="dst"
 low_level_calls="low_level.sh"
 chr_info="chromo_length_info.txt"
-bam_prefx="indexing/indexed/chr"
-indx_prefx="indexing/indexed/chr"
+bam_prefx="/home/andrew/school/dnavis/jbrowse/donors/NA18507/chr"
+indx_prefx="/home/andrew/school/dnavis/jbrowse/donors/NA18507/chr"
 
 $front_end_dir/biosql < $input_sql > $interm_code
 
