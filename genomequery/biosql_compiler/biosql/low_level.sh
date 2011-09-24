4e78a7df06258402d5cf1f164715cb786b5f4f7e
andrew <andrew@rodion.(none)>
2011-09-23 18:04:52 -0700

    asdfasdf

--- genomequery/biosql_compiler/biosql/low_level.sh
+++
@@ -1,9 +0,0 @@
-['code_generator.py', 'bytecode.txt', 'dst/chr1', 'front_end', 'back_end', 'dst', '/home/andrew/jbrowse/donors/NA18507/chr1.bam', '/home/andrew/jbrowse/donors/NA18507/chr1.bam.mates.indx', 'chr1', '247249719']
-back_end/all_tools load_table dst/genes.txt dst/chr1/genes 1 2 3 4 chr1
-cp H2isvalid_single.c back_end/isvalid_single.c
-make
-back_end/all_tools single_range /home/andrew/jbrowse/donors/NA18507/chr1.bam /home/andrew/jbrowse/donors/NA18507/chr1.bam.mates.indx 247249719 dst/chr1/H2 0
-back_end/all_tools intersect dst/chr1/genes.table imported /home/andrew/jbrowse/donors/NA18507/chr1.bam.mates.indx bam_single /home/andrew/jbrowse/donors/NA18507/chr1.bam dst/chr1/H3 2 247249719 dst/chr1/genes dst/chr1/H2 dummy
-cp outisvalid_join.c back_end/isvalid_join.c
-make
-back_end/all_tools intersect dst/chr1/genes.table imported /home/andrew/jbrowse/donors/NA18507/chr1.bam.mates.indx bam_single /home/andrew/jbrowse/donors/NA18507/chr1.bam dst/chr1/out 5 247249719 dummy dummy dst/chr1/H3
